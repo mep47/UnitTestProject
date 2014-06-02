d9908d82a2a6ee17e1f06a7466f224cd1d347120
Mike Price <mike.mep.price@gmail.com>
2014-06-02 11:09:39 -0400

    test

--- src/unittest/UnitTest1.java
+++ src/unittest/UnitTest1.java
@@ -1,13 +1,24 @@
 package unittest;
 
+import static org.junit.Assert.*;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.junit.Test;
+
 public class UnitTest1 {
 	
 static public void main(String[] args)
+{
+	UnitTest1 unitTest1 = new UnitTest1();
+	unitTest1.test1();
+}
+
+@Test
+public void test1()
 {
 	System.out.println("Running UnitTest1");
 	File file = new File("/var/lib/jenkins/workspace/JenkinsTest/build/jar/JenkinsTest.jar");
@@ -31,7 +42,7 @@ static public void main(String[] args)
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        System.out.println(line);
+        assertEquals("The results of test1 must match'", "Jenkins Test", line);
 }
 
 }
