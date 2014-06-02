14d094a3d9f5b99420423558d3ad92454174818c
Mike Price <mike.mep.price@gmail.com>
2014-06-02 09:58:24 -0400

    add processbuilder to UnitTest1.java

--- src/unittest/UnitTest1.java
+++ src/unittest/UnitTest1.java
@@ -9,28 +9,25 @@ public class UnitTest1 {
 static public void main(String[] args)
 {
 	System.out.println("Running UnitTest1");
-//    ProcessBuilder builder = new ProcessBuilder(
-//            "/var/lib/jenkins/workspace/JenkinsTest/build/jar/Jenkins Test.jar");
-//        builder.redirectErrorStream(true);
-//        Process p = null;
-//		try {
-//			p = builder.start();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//        String line = null;
-//        while (true) {
-//            try {
-//				line = r.readLine();
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//            if (line == null) { break; }
-//            System.out.println(line);
-//        }
+    ProcessBuilder builder = new ProcessBuilder(
+            "java -jar /var/lib/jenkins/workspace/JenkinsTest/build/jar/JenkinsTest.jar");
+        builder.redirectErrorStream(true);
+        Process p = null;
+		try {
+			p = builder.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = null;
+        try {
+			line = r.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(line);
 }
 
 }
